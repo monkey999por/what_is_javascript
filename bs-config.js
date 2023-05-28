6887b488ecedc5daed70e29a0f9d6cd662022e52
monkey999 <yururi1122@gmail.com>
2023-05-28 18:01:51 +0900

    ok

--- bs-config.js
+++ bs-config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  files: ["./MDN/**"], //監視するファイルを指定
+  files: ["./MDN/**", "./content/**", "./ts/**"], //監視するファイルを指定
   server: {
     //デフォルトで開くファイルを指定
     baseDir: "./MDN/Asynchronous", //ファイルまでのディレクトリのパス
     index: "index.html", //ファイル名
   },
+  whatchEvents: ["add", "change"],
   proxy: false,
   port: 8000, //使用するポート番号
 };
